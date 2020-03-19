2f481dc9d7baae857a778aea671f5fc6104441e6
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2020-03-19 12:12:30 -0600

    Implemented basic sender

---
+++ network_info/network-info.go
@@ -0,0 +1,11 @@
+package network_info
+
+const (
+	SENDER_IP = "localhost"
+	SENDER_PORT = 12345
+	SENDER_FULL = SENDER_IP + ":" + string(SENDER_PORT)
+	
+	RECEIVER_IP = "localhost"
+	RECEIVER_PORT = 12345
+	RECEIVER_FULL = RECEIVER_IP + ":" + string(RECEIVER_PORT)
+)
--- sender.go
+++ sender.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"net"
+	"protobuf-networking/network_info"
+	"protobuf-networking/protobuf/protobuild/SimpleMsg"
+)
+
+type Sender struct {
+	socket net.Conn
+}
+
+func (sender *Sender) Send() error {
+	msg := &SimpleMsg.SimpleMsg{SimpleString: "Test msg"}
+	serializedMsg, err := proto.Marshal(msg)
+	if err != nil {
+		fmt.Println("Failed to Marshal msg", msg)
+	}
+	_, err = sender.socket.Write(serializedMsg)
+	return err
+}
 
 func main() {
-	fmt.Println("Hello from sender")
+	fmt.Println("Starting sender...")
+	conn, err := net.Dial("tcp", network_info.RECEIVER_FULL)
+	if err != nil {
+		fmt.Println("Failed to establish TCP connection to", network_info.RECEIVER_FULL, "with error", err)
+	}
+	sender := &Sender{conn}
+	err = sender.Send()
+	if err != nil {
+		fmt.Println("Failed to send msg with error:", err)
+	}
 }
